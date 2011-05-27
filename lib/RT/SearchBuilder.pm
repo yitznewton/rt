fc2a2e885360abf5d1ad1445b70f8636fb8b807b
Ruslan Zakirov <ruz@bestpractical.com>
2011-05-27 22:58:42 +0400

    delete split of FUNCTION into ALIAS/FIELD arguments
    it was only way to write a function on left hand side,
    now Limit in SearchBuilder has FUNCTION argumnt and we
    can avoid this mispractice

--- lib/RT/SearchBuilder.pm
+++ lib/RT/SearchBuilder.pm
@@ -306,10 +306,7 @@ sub Limit {
         $ARGS{'VALUE'} = 'NULL';
     }
 
-    if ($ARGS{FUNCTION}) {
-        ($ARGS{ALIAS}, $ARGS{FIELD}) = split /\./, delete $ARGS{FUNCTION}, 2;
-        $self->SUPER::Limit(%ARGS);
-    } elsif ($ARGS{FIELD} =~ /\W/
+    if ($ARGS{FIELD} =~ /\W/
           or $ARGS{OPERATOR} !~ /^(=|<|>|!=|<>|<=|>=
                                   |(NOT\s*)?LIKE
                                   |(NOT\s*)?(STARTS|ENDS)WITH
