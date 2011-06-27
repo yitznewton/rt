3a5fa991572cec6c0d8b159c4b5b78beed34582b
Ruslan Zakirov <ruz@bestpractical.com>
2011-06-27 23:19:41 +0400

    first pass at caculating stats after querying DB

--- lib/RT/Report/Tickets.pm
+++ lib/RT/Report/Tickets.pm
@@ -454,9 +454,7 @@ sub SetupGroupings {
             }
         }
         elsif ( $e->{'META'}{'Calculate'} ) {
-            # ....
-        }
-        else {
+            $e->{'NAME'} = 'postfunction'. $self->{'postfunctions'}++;
         }
         push @{ $res{'Functions'} }, $e->{'NAME'};
         $column_info{ $e->{'NAME'} } = $e;
@@ -483,6 +481,7 @@ sub _DoSearch {
         );
     }
     else {
+        $self->CalculatePostFunctions;
     }
 }
 
@@ -592,6 +591,40 @@ sub SortEntries {
         sort __sort_function_we_need_named @data
     ];
 } }
+
+sub CalculatePostFunctions {
+    my $self = shift;
+
+    my $info = $self->{'column_info'};
+    foreach my $column ( values %$info ) {
+        next unless $column->{'TYPE'} eq 'statistic';
+        next unless $column->{'META'}{'Calculate'};
+
+        $self->CalculatePostFunction( $column );
+    }
+}
+
+sub CalculatePostFunction {
+    my $self = shift;
+    my $info = shift;
+
+    my $code = $self->FindImplementationCode( $info->{'META'}{'Calculate'} );
+    unless ( $code ) {
+        # TODO: fill in undefs
+        return;
+    }
+
+    my $column = $info->{'NAME'};
+
+    my $base_query = $self->{'_sql_query'};
+    foreach my $item ( @{ $self->{'items'} } ) {
+        $item->{'values'}{$column} = $code->(
+            $self,
+            Query => join( ' AND ', grep defined && length, $base_query, $item->Query ),
+        );
+        $item->{'fetched'}{$column} = 1;
+    }
+}
 
 sub GenerateDateFunction {
     my $self = shift;
--- lib/RT/Report/Tickets/Entry.pm
+++ lib/RT/Report/Tickets/Entry.pm
@@ -127,6 +127,7 @@ sub Query {
             push @parts, "$field $op $value";
         }
     }
+    return () unless @parts;
     return join ' AND ', grep defined && length, @parts;
 }
 
